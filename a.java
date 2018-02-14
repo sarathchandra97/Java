c925f8c725997853748b608381b50fc66e6c5060
Yakkati <syakka@amazon.com>
2018-02-14 12:17:43 +0530

    this is my first commit

---
+++ a.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class A{
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args){
+		System.out.println("hello world!!!")
+	
+	}
+
+}
+ 
